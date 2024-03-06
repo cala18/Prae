f8a781f5efc1d2bc1f9eaed3545b34ee0b7c22cb
cala18 <brayancala2375@gmail.com>
2024-03-06 15:16:20 -0500

    style: :art: Cala

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};
+
+// // JavaScript
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+// let menuOpen = false;// Variable para rastrear el estado del menú
+
+// menu.addEventListener("click", function () {
+//   navbar.classList.toggle(".active");
+//   menuOpen = !menuOpen; // Cambiar el estado del menú al hacer clic
+// });
+
+// window.onscroll = () => {
+//   if (!menuOpen) {
+//     // Solo si el menú no está abierto, entonces remueve la clase "active"
+//     navbar.classList.remove(".active");
+//   }
+// };
+
+
+
